0eccd134edda3f8ed05f3706e24e5693356e1b06
Marina <m.iriboshii@outlook.com>
2023-11-19 21:54:22 -0300

    transformação em tabela

--- principal.py
+++ principal.py
@@ -1,10 +1,19 @@
 from pyspark.sql import SparkSession
 
-spark = SparkSession.builder.appName('Curso de Pyspark').getOrCreate()
+spark = (SparkSession.builder.appName('Curso de Pyspark')
+		.config('spark.sql.repl.eagerEval.enabled', True)
+        .getOrCreate()
+         )
 
 print ("Deu certo! Parabains")
 
-
-df = spark.read.csv('./DATASETS/LOGINS.csv', sep=';', header = True)
+#formata os campos e lê .csv, mas não retorna em formato de tabela
+#campos muito longos quebrados
+df = spark.read.csv('./DATASETS/LOGINS.csv', sep=';', header=True)
 df.show()
+
+
+#transformação em tabela
+#spark.conf.set('spark.sql.repl.eagerEval.enabled', True)
+df
 
